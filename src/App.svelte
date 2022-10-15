95bb1f65f7aaeadd29859cfdaea6705166e30d2d
FROSSTY <aom_jackal@hotmail.com>
2022-10-16 03:13:39 +0700

    secound commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,152 @@
 <script>
   // import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import Icons from './lib/Icons.svelte'
+  import plusIcon from './assets/plus.png'
+
+  let newItem = "" ;
+  let todoList = [] ;
+
+  function addTodo() {
+    if(newItem != ""){
+      todoList = [
+        ...todoList,
+      {
+        task: newItem,
+        completed: false
+      },
+    ];
+    newItem = "" ;
+    }
+  }
+
+// function remove
+function removeFromList(index){
+  todoList.splice(index, 1);
+  todoList = todoList;
+}
 </script>
 
 <main>
-  <div>
-  
-    <div class="card">
-      <Counter />
-    </div>
-    <div>
-      <p>Hello</p>
-      <h1>Hello</h1>
-    </div>
-  </div>
 
+    <!-- <div class="card">
+      <Counter />
+    </div> -->
+    <!-- <div>
+      <h1>Hello</h1>
+    </div> -->
+    <div>
+      <form on:submit|preventDefault={addTodo}>
+        <input bind:value={newItem} class="todos_inputs" placeholder="Input todo" type="task"/>
+        <button  class="todos_plusButton">Add</button>
+      </form>
+      <h2 class="todos_listHeader">Todos</h2>
+      {#each todoList as item, index }
+        <div class="todo">
+          <span class={`todo_text ${item.completed ? "todo_checked--strike" : ""}`}>{item.task}</span>
+        
+        <div class="icons">
+          <button class="icon_button" on:click={() => (item.completed = !item.completed)}>
+            <Icons name="check-mark" class="icon"/>
+          </button>
+
+          <button class="icon_button" on:click={()=> removeFromList(index)}>
+            <Icons name="delete" class="icon"/> 
+          </button>
+        </div>
+      </div>
+      {/each}
+    </div>
 </main>
 
 <style>
+  /* .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 90vh;
+    background: #222e50 url(./assets/plus.png) no-repeat;
+    background-size: cover; 
+    padding-top: 10vh;
+
+  } */
+  .todo{
+    display: flex;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 0 15px rgb(0,0,0 /20%);
+    /* background-color: hsla(0, 0%, 100, 0.2); */
+    background-color: hsla(0, 0%, 100%, 0.2);
+    -webkit-backdrop-filter: blur(25px);
+    backdrop-filter: blur(25px);
+     width: inherit;
+     margin-top: 15px;
+     font-size: 1.2rem;
+     justify-content: space-between;
+     align-items: center;
+
+  }
+.todos_listHeader {
+  text-align: center;
+  padding: 20px;
+  border-radius: 20px;
+  box-sizing:  0 0 12px rgb(0 0 0 / 20%);
+  background-color: hsla(0, 0%, 100%, 0.2);
+  -webkit-backdrop-filter: blur(25px);
+  backdrop-filter: blur(25px);
+  margin: 15px 0px 20px 0px;
+  font-size: 1.2rem;
+
+}
+.todos_inputs{
+  background-color: inherit;
+  border: none;
+  box-shadow: none;
+  text-decoration: none;
+  font-size: 1.2rem;
+  border-bottom: 1px solid black;
+  margin-top: 15px;
+  outline: none;
+  width: 500px;
+  color: black;
+
+
+}
+.todos_button{
+  background-color: inherit;
+  border: none;
+  box-shadow: none;
+  font-size: 1.2rem;
+  cursor: pointer;
+
+}
+.todos_plusButton {
+  background-color: inherit;
+  border: none;
+  box-shadow: none;
+  font-size: 1.2rem;
+  cursor: pointer;
+  /* min-height: 4vh; */
+  /* background: white url(./assets/plus.png) no-repeat; */
+  background-size: cover; 
+  padding-top: 1vh;
+  color: black;
+ 
+}
+.icon_button{
+  background-color: transparent;
+  border:none;
+  box-shadow: none;
+  font-size: 1.2rem;
+  cursor: pointer;
+  color: rgb(0, 0, 0,0.54);
+}
+.icon {
+  background: rgb(0, 0, 0,0.54);
+} 
+.todo_checked--strike{
+  text-decoration: line-through;
+}
+
+
 </style>
---
+++ src/lib/Icons.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let name;
+    export let width = "1.5rem"
+    export let height = "1.5rem"
+    export let focusable = false
+
+    let icons = [
+        {
+            box:24,
+            name: "check-mark",
+            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.041 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591zm-5.041-15c-1.654 0-3 1.346-3 3v14c0 1.654 1.346 3 3 3h14c1.654 0 3-1.346 3-3v-14c0-1.654-1.346-3-3-3h-14zm19 3v14c0 2.761-2.238 5-5 5h-14c-2.762 0-5-2.239-5-5v-14c0-2.761 2.238-5 5-5h14c2.762 0 5 2.239 5 5z"/></svg>`,
+        },
+        {
+            box: 32,
+            name: "delete",
+            svg: `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m3-19h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>`
+        }
+    ]
+    let displayIcon = icons.find((e) => e.name == name)
+</script>
+
+<svg class={$$props.class}
+{focusable}
+{width}
+{height}
+viewBox="0 0 {displayIcon.box} {displayIcon.box}">{@html displayIcon.svg}</svg>
