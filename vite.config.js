ecd7cc31f8d5384da3afe0b7f9ca24b9932aaa2d
FROSSTY <aom_jackal@hotmail.com>
2022-10-17 16:27:24 +0700

    static.json up

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import smelte from 'smelte/rollup-plugin-smelte'
-// import {pcss} from 'smelte/rollup-plugin-smelte'
-// import production from process.env.ROLLUP_WATCH
-// import production from '@fullhuman/postcss-purgecss'
+// import node from "@sveltejs/adapter-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
